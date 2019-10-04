0420120a9c280d6a26eccf699852bc37308c97db
jayalakshmi97 <jayalaskhmiajayakumar97@gmail.com>
2019-10-04 12:06:53 +0530

    nil

--- app/src/main/java/com/example/gitproject/MainActivity.java
+++ app/src/main/java/com/example/gitproject/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.gitproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+Button b1,b2;
+ImageView imgview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imgview=findViewById(R.id.imgview);
+        b1=findViewById(R.id.btn_cam);
+        b2=findViewById(R.id.btn_save);
     }
 }
